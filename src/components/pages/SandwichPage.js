07c457d2d0354f3b1582894d5126e944248ad81c
Brandon Wang <315bwang@gmail.com>
2021-01-27 17:11:02 -0500

    Improve SandwichPage; add nutrition facts.

--- src/components/pages/SandwichPage.js
+++ src/components/pages/SandwichPage.js
@@ -1,4 +1,7 @@
+import "./../../styles.css"
 import React, { Component } from "react";
+
+import NutritionFacts from "../modules/NutritionFacts"
 
 import PropTypes from 'prop-types';
 
@@ -7,36 +10,44 @@ class SandwichPage extends React.Component {
 
         return (
             <div class="container">
-                <div class="row">
+                <div class="row Page-title">
                     <div class="col-sm">
                         <h1>{this.props.sandwich.title}</h1>
                     </div>
                 </div>
                 <div class="row">
-                    Tags: {this.props.sandwich.tags.join(", ")}
-                </div>
-                <div class="row">
-                    Blurb: {this.props.sandwich.short_description}
-                </div>
-                <div class="row">
-                    Grade versions:
-                </div>
-                <div class="row">
-                    Resources:
-                    {this.props.sandwich.resources.map((resource) => <div class="container">
-                        <a href={process.env.PUBLIC_URL+resource.url}>{resource.text}</a>
-                    </div>)}
-                    <div class="container">
-                            <a href={process.env.PUBLIC_URL+"/sandwich/"+this.props.sandwich.uid+"/sandwich-"+this.props.sandwich.uid+".zip"} class="btn btn-info" role="button">.ZIP File</a>
+                    <div class="col-9 text-left">
+
+                        <div class="row">
+                            Tags: {this.props.sandwich.tags.join(", ")}
+                        </div>
+                        <div class="row">
+                            Blurb: {this.props.sandwich.short_description}
+                        </div>
+                        <div class="row">
+                            Grade versions:
+                        </div>
+                        <div class="row">
+                            Related Modules:
+                        </div>
+                        <div class="row text-center">
+                            <div class="col">
+                                <h4> Resources:</h4>
+                                {this.props.sandwich.resources.map((resource) => <div class="container">
+                                    <a href={process.env.PUBLIC_URL + resource.url}>{resource.text}</a>
+                                </div>)}
+                                <div class="container">
+                                    <a href={process.env.PUBLIC_URL + "/sandwich/" + this.props.sandwich.uid + "/sandwich-" + this.props.sandwich.uid + ".zip"} class="btn btn-info" role="button">.ZIP File</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <NutritionFacts sandwichData={[this.props.sandwich]} />
                     </div>
                 </div>
-                <div class="row">
-                    Nutrition Facts:
-                </div>
 
-                <div class="row">
-                    Related Modules:
-                </div>
             </div>
         )
     }
