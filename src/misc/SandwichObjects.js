67807b4e13ef8fdcc41b279e5e106b64195d424c
Brandon Wang <bdnwang@mit.edu>
2021-03-19 10:10:18 -0400

    Forgot to add src/misc before.

---
+++ src/misc/SandwichObjects.js
@@ -0,0 +1,17 @@
+export class CustomSandwichData {
+  constructor(title, numSlots, requiredFillings, optionalFillings) {
+    this.title = title;
+    this.numSlots = numSlots;
+    this.requiredFillings = requiredFillings;
+    this.optionalFillings = optionalFillings;
+  }
+}
+
+export class SandwichFillingData {
+    constructor(title, isRequired, index) {
+      this.title = title;
+      this.isRequired = isRequired;
+      this.index = index;
+    }
+  }
+  
