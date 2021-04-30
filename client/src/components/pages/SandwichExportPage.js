1306ca4b7df2905ed8c5271fab8c89d847214138
Brandon Wang <bdnwang@mit.edu>
2021-04-30 17:44:57 -0400

    Layout sandwich export page.

--- client/src/components/pages/SandwichExportPage.js
+++ client/src/components/pages/SandwichExportPage.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
 import styled from "styled-components";
+import WeeklySandwichNutritionFacts from "../modules/sandwich-builder-v2/WeeklySandwichNutritionFacts";
 
 const InfoBox = styled.div`
     border: 1px solid lightgray;
@@ -16,33 +17,83 @@ const MainHeading = styled.h1`
     margin-left: 10%;
 `;
 
-const Subheading = styled.h4`
+const ExportedContent = styled.div`
+    background-color: #8bff61;
+    margin: 30px;
+    padding: 15px;
     text-align: left;
+
 `;
 
-const LearningGoalCategory = styled.div`
-    margin-bottom: 20px;
+const ExportedContentDescription = styled.div`
+    color: black;
+    font-size: 20px;
 `;
 
-const LearningGoalContent = styled.div`
-    text-align: left;
+const LinkTextField = styled.div`
+    background-color: #bfffa8;
+    border: 1px solid gray;
+    padding: 5px;
+    width: 100%;
+    font-family: monospace;
 `;
 
-const LearningSubgoals = styled.div`
-    font-size: small;
-`;
-
-const BuilderContainer = styled.div`
-    border: 1px solid lightgray;
-    padding: 20px;
+const ZipButton = styled.button`
+    padding: 10px;
+    margin-top: 20px;
 `;
 
 class SandwichExportPage extends React.Component {
     render() {
         return (
-                <>
+            <>
+                <div className="row">
                     <MainHeading>{this.props.sandwichData.title}</MainHeading>
-                </>
+                </div>
+                <div className="row">
+                    <div className="col-9">
+                        <ExportedContent className="row">
+                            <ExportedContentDescription className="col-4">
+                                Class slides:
+                            </ExportedContentDescription>
+                            <LinkTextField className="col">
+                                https://reactjs.org/docs/refs-and-the-dom.html
+                            </LinkTextField>
+                        </ExportedContent>
+                        <ExportedContent className="row">
+                            <ExportedContentDescription className="col-4">
+                                Student Workbook:
+                            </ExportedContentDescription>
+                            <LinkTextField className="col">
+                                https://reactjs.org/docs/refs-and-the-dom.html
+                            </LinkTextField>
+                        </ExportedContent>
+                        <ExportedContent className="row">
+                            <ExportedContentDescription className="col-4">
+                                Teacher Facilitation Guide:
+                            </ExportedContentDescription>
+                            <LinkTextField className="col">
+                                https://reactjs.org/docs/refs-and-the-dom.html
+                            </LinkTextField>
+                        </ExportedContent>
+                    </div>
+                    <div className="col-3">
+                        <WeeklySandwichNutritionFacts sandwichId={[this.props.sandwichId]} />
+                    </div>
+                </div>
+                <div className="row">
+                    <ExportedContent className="col-9">
+                        <h4>Additional Resources:</h4>
+                        {this.props.sandwichData.resources.map((resource, index) =>
+                            <div>
+                                <a key={index} href={process.env.PUBLIC_URL + resource.url}>{resource.text}</a>
+                            </div>
+                        )}
+                        <ZipButton className="btn btn-primary">Download ZIP</ZipButton>
+                    </ExportedContent>
+                </div>
+
+            </>
         )
     }
 }
